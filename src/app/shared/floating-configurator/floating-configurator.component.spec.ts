8c02cf4448d83d19c8d0f7f07679c13240ee13fa
Andrés Beltrán <wwandresbeltran@gmail.com>
2025-02-21 22:37:18 -0500

    Estructuración inicial del proyecto. Se extrae el layout del app como componentes

---
+++ src/app/shared/floating-configurator/floating-configurator.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FloatingConfiguratorComponent } from './floating-configurator.component';
+
+describe('FloatingConfiguratorComponent', () => {
+  let component: FloatingConfiguratorComponent;
+  let fixture: ComponentFixture<FloatingConfiguratorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FloatingConfiguratorComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FloatingConfiguratorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/topbar/topbar.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { StyleClassModule } from 'primeng/styleclass';
+import { ConfiguratorComponent } from '../configurator/configurator.component';
+import { LayoutService } from '../../core/services/layout.service';
+
+@Component({
+  selector: 'app-topbar',
+  standalone: true,
+  imports: [RouterModule, CommonModule, StyleClassModule, ConfiguratorComponent],
+  templateUrl: './topbar.component.html',
+  styleUrl: './topbar.component.scss'
+})
+export class TopbarComponent {
+
+  items!: MenuItem[];
+
+  constructor(public layoutService: LayoutService) { }
+
+  toggleDarkMode() {
+    this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
+  }
+
+}
